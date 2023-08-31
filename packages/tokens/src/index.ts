0fa0434998c4d0d9b001f15a8b25e11f0b66a132
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-08-31 10:49:49 -0300

    Configurando mais tokens
    Aula com apenas download e edição para o Design System

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,2 +1,10 @@
 export * from './colors';
-// index para não precisar colocar o arquivo colors a mais na importação. Pois este arquivo que exporta é index.
+// index para não precisar colocar o arquivo colors a mais na importação. Pois este arquivo que exporta é index. O mesmo vale para o resto:
+
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
