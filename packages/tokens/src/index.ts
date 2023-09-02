a8a8f939ac201f4c07cdad622c4faf150d060a7f
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-09-02 15:47:52 -0300

    Configurando eslint
    Primeira vez que vou usar o modelo da Rocket.

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,4 +1,4 @@
-export * from './colors';
+export * from './colors'
 // index para não precisar colocar o arquivo colors a mais na importação. Pois este arquivo que exporta é index. O mesmo vale para o resto:
 
 export * from './space'
